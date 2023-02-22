e3babaa374b84df373569defa21cd4dd5e11b3e3
IshmaelSerrano <ishmael.serrano.67@gmail.com>
2023-02-22 11:58:46 +0000

    aws

--- src/main/java/com/ishmael/springbootcrudoperation/SpringBootCrudOperationApplication.java
+++ src/main/java/com/ishmael/springbootcrudoperation/SpringBootCrudOperationApplication.java
@@ -1,7 +1,11 @@
 package com.ishmael.springbootcrudoperation;
 
+import com.ishmael.springbootcrudoperation.entity.Books;
+import com.ishmael.springbootcrudoperation.repository.BooksRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootCrudOperationApplication {
@@ -9,5 +13,6 @@ public class SpringBootCrudOperationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrudOperationApplication.class, args);
 	}
+
 
 }
